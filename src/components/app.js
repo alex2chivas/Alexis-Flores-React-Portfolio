ff6bbfaaf4843ee8f2517e148e1e4a8f0bcd115e
Alexis Flores <alex2chivas@hotmail.com>
2020-01-09 16:07:14 -0800

    all the new folders, pages, navigation, and scss files

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
-import moment from "moment";
+import {
+  BrowserRouter as Router, 
+  Switch, 
+  Route
+} from 'react-router-dom';  
+
+
+
+import NavigationContainer from './navigation/navigation-container';
+import Home from './pages/home';
+import About from './pages/about';
+import Contact from './pages/contact';
+import Blog from './pages/blog';
+import PortfolioDetail from "./portfolio/portfolio-details";
+import Auth from "./pages/auth";
+import NoMatch from "./pages/no-match";
+
+
 
 export default class App extends Component {
+
   render() {
     return (
-      <div className='app'>
-        <h1>Portfolio</h1>
-        <h2>React Redux Router</h2>
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
+      <div className='container'>
+
+        <Router>
+          <div>
+            <NavigationContainer/>
+ 
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/auth" component={Auth}/>
+              <Route path="/about-me" component={About}/>
+              <Route path="/contact-me" component={Contact}/>
+              <Route path="/blog" component={Blog}/>
+              <Route exact path="/portfolio/:slug" component={PortfolioDetail}/>
+              <Route component={NoMatch}/>
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
