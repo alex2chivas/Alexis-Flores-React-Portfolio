e23eaa4312e3d4d334b615f6955905faaa595c68
Alexis Flores <alex2chivas@hotmail.com>
2020-01-10 17:54:24 -0800

    added the entire implementation for authorization engine for pages/portolio

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -6,7 +6,7 @@ export default class Login extends Component {
     super(props);
 
     this.state = {
-      email: "",
+      email: "", 
       password: "",
       errorText: ""
     };
@@ -15,7 +15,8 @@ export default class Login extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   // alexrunner002@gmail.com
-  //Runner1994@
+  //Runner1994@ 
+  //health4me app.
 
   handleChange(event) {
     this.setState({
@@ -40,17 +41,22 @@ export default class Login extends Component {
     ).then(response => {
       if (response.data.status === 'created') {
         console.log("You can come in...")
+        this.props.handleSuccessfulAuth();
       } else {
         this.setState({
           errorText: "Wrong email or password"
-        })
+        });
+        this.handleUnsuccessfulAuth();
       }
     })
     .catch(error => {
       this.setState({
         errorText: "An error occured"
-      })
+        
+      });
+      this.handleUnsuccessfulAuth();
     });
+
     event.preventDefault();
   }
 
--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -4,6 +4,24 @@ import Login from "../auth/login";
 import loginImg from '../../../static/assets/images/auth/login.jpg'
 
 export default class Auth extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+        this.handleUnsuccessfulAuth = this.handleUnsuccessfulAuth.bind(this);
+        
+    }
+
+    handleSuccessfulAuth() {
+        this.props.handleSuccessfulLogin();
+        this.props.history.push("/");
+        // history of our browsing, access our application, connecting routes
+    }
+
+    handleUnsuccessfulAuth() {
+        this.props.handleUnsuccessfulLogin();
+    }
+
     render() {
         return (
 
@@ -15,7 +33,10 @@ export default class Auth extends Component {
                 />
 
                 <div className="right-column">
-                    <Login/>
+                    <Login 
+                    handleSuccessfulAuth={this.handleSuccessfulAuth}
+                    handleUnsuccessfulAuth={this.handleUnsuccessfulAuth}
+                    />
                 </div>
 
             </div>
