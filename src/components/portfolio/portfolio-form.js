7179063c6410100874d3d8fdc057826e598868d9
Alexis Flores <alex2chivas@hotmail.com>
2020-01-17 17:27:15 -0800

    added axio.patch with modified config options into react-portfolio

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -17,7 +17,11 @@ export default class PortfolioForm extends Component {
       url: "",
       thumb_image: "",
       banner_image: "",
-      logo: ""
+      logo: "",
+      editMode: false, // Note // This helps create dynamic axio request with personalize Config setting in axios
+      apiUrl: "https://alexisflores.devcamp.space/portfolio/portfolio_items",
+      apiAction: 'post'
+
     };
 
     this.handleChange = this.handleChange.bind(this)
@@ -56,10 +60,12 @@ export default class PortfolioForm extends Component {
         description: description || "",
         category: category || "eCommerce",
         position: position || "",
-        url: url || ""
-  
-      })
-    }
+        url: url || "",
+        editMode: true,
+        apiUrl: `https://alexisflores.devcamp.space/portfolio/portfolio_items/${id}`,
+        apiAction: "patch"
+      });
+    };
   }
 
   handleThumbDrop() {
@@ -129,18 +135,19 @@ export default class PortfolioForm extends Component {
   }
 
   handleSubmit(event) {
-    axios.post("https://alexisflores.devcamp.space/portfolio/portfolio_items", 
-    this.buildForm(), 
-    {
-      withCredentials: true
-    })
+  axios({
+    method: this.state.apiAction,
+    url: this.state.apiUrl,
+    data: this.buildForm(),
+    withCredentials: true
+  })
     .then(response => {
       this.props.handleSuccessfulFormSubmission(response.data.portfolio_item);
 
       this.setState ({ // Note //Highly discourage to do State manually on .then() function after a API call
         name:"",        // Note // This are only our state and we need to learn the form
         description: "",
-        category: "eCommerce",
+        category: "eCommerce",  
         position: "",
         url: "",
         thumb_image: "",
