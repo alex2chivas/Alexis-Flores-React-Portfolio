7d2cfe87ecc62f41f96662638f00023d954fed4a
Alexis Flores <alex2chivas@hotmail.com>
2020-01-20 23:47:06 -0800

    added api call and other functionality to blog-detail and created style sheet for blog

--- src/components/app.js
+++ src/components/app.js
@@ -124,7 +124,7 @@ export default class App extends Component {
               <Route path="/about-me" component={About}/>
               <Route path="/contact-me" component={Contact}/>
               <Route path="/blog" component={Blog}/> 
-              <Route path="/b/:slug" component={BlogDetail}/> 
+              <Route path="/b/:slug" component={BlogDetail} /> 
               {this.state.loggedInStatus === "LOGGED_IN" ? this.authorizedPages() : null}
               <Route exact path="/portfolio/:slug" component={PortfolioDetail}/>
               <Route component={NoMatch}/>
--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,10 +1,51 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 export default class BlogDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentId: this.props.match.params.slug, // Note // We are calling props from the route not from the blog file. 
+      blogItem: {}
+    }
+  }
+
+  getBlogItem() {
+    axios.get(`https://alexisflores.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
+    ).then(response => {
+      this.setState({
+        blogItem: response.data.portfolio_blog
+      })
+    }).catch(error => {
+      console.log("getBlogItem error", error);
+    })
+  }
+
+  componentDidMount() {
+    this.getBlogItem();
+  }
+
   render() {
+    const {title, 
+      content, 
+      featured_image_url, 
+      blog_status} = this.state.blogItem
+
     return (
-      <div>
-        <h1>Blog detail</h1>
+      <div className="blog-container">
+        <div className='content-container'>  
+          <div className="title">
+            <h1>{title}</h1>
+          </div>
+          
+          <div className='featured-image-wrapper'>
+            <img src={featured_image_url}/>
+          </div>
+
+          <div className='content'>
+            {content}
+          </div>
+        </div>  
       </div>
     );
   }
