0e3ce6276557c1afea0ea825761b38bad8a11329
Alexis Flores <alex2chivas@hotmail.com>
2020-01-22 18:34:25 -0800

    [added]:blog form into the blod modal

---
+++ src/components/blog/blog-form.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default class BlogForm extends Component {
+  render() {
+    return (
+      <from>
+        <input type="text" />
+        <input type="text" />
+
+        <button>Save</button>
+      </from>
+    );
+  }
+}
--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import ReactModal from 'react-modal';
 
+import BlogForm from "../blog/blog-form"
+
 ReactModal.setAppElement(".app-wrapper"); // Note // This is to remove the warning in the console so screen readers do not have a problem 
 
 export default class BlogModal extends Component {
@@ -12,9 +14,10 @@ export default class BlogModal extends Component {
         top                   : '50%',
         left                  : '50%',
         right                 : 'auto',
+        bottom                : '-50px',
         marginRight           : '-50%',
         transform             : 'translate(-50%, -50%)',
-        width                 : '700px'
+        width                 : '675px'
       },
       overlay: {
         backgroundColor       : 'rgba(1, 1, 1, 0.85)',
@@ -31,7 +34,7 @@ export default class BlogModal extends Component {
         this.props.handleModalClose();
       }} 
       isOpen={this.props.modalIsopen}>
-        <h1>I am in a modal!</h1>
+        <BlogForm/>
       </ReactModal>
     )
   }
