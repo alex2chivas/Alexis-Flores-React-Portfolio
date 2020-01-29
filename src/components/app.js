d41609a51a3c8f40b22e8a7d7c7bb8754d801b27
Alexis Flores <alex2chivas@hotmail.com>
2020-01-29 15:10:45 -0800

    [added]: a dedicated icons function

--- src/components/app.js
+++ src/components/app.js
@@ -5,9 +5,7 @@ import {
   Route
 } from 'react-router-dom';  
 import axios from "axios";
-import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faSignOutAlt, faEdit, faMinusCircle, faSpinner, faPlusSquare } from "@fortawesome/free-solid-svg-icons";
 
 import NavigationContainer from './navigation/navigation-container';
 import Home from './pages/home';
@@ -19,12 +17,13 @@ import PortfolioDetail from "./portfolio/portfolio-details";
 import PortfolioManager from "./portfolio/portfolio-manager";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
-
-library.add(faTrash, faSignOutAlt, faEdit, faMinusCircle, faSpinner, faPlusSquare)
+import Icons from "../helpers/icons"
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+
+    Icons();
 
     this.state = {
       loggedInStatus: "NOT_LOGGED_IN"
