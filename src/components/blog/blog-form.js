84051d9c2d364fe6b6a36fd829079ee249ec876e
Alexis Flores <alex2chivas@hotmail.com>
2020-01-29 16:55:21 -0800

    [added]: styles for rich-draft-wysiwyg

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import axios from "axios";
 
+import RichTextEditor from "../forms/rich-text-editor"
+
 export default class BlogForm extends Component {
   constructor(props) {
     super(props);
@@ -64,6 +66,10 @@ export default class BlogForm extends Component {
               <option value="draft">Draft</option>
               <option value="published">Published</option>
           </select>
+        </div>
+        
+        <div className="one-column">
+          <RichTextEditor/>
         </div>
 
         <button className="btn">Save</button>
