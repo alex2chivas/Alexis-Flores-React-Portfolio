93bf6347bbd20b90f2ac4633cd3306d78ca5bc6d
Alexis Flores <alex2chivas@hotmail.com>
2020-02-25 15:20:43 -0800

    [added]: new piece of setstate if there is a filter in place when making the axios call & [added]: all option on the render method

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -17,21 +17,25 @@ export default class PortfolioContainer extends Component {
     };
 
     handleFilter(filter) {
-        this.setState({
-            data: this.state.data.filter( item => {
-                return item.category === filter;
-            })
-        })   
+        filter === "CLEAR_FILTERS" ? this.getPortfolioItems(): this.getPortfolioItems(filter);
     };
 
-    getPortfolioItems() {
+    getPortfolioItems(filter = null) {
         axios
           .get('https://alexisflores.devcamp.space/portfolio/portfolio_items')
           .then( response => {
-            this.setState({
-                data: response.data.portfolio_items
-            })
-          })
+            if(filter) {
+                this.setState({
+                    data: response.data.portfolio_items.filter( item => {
+                        return item.category === filter;
+                    })
+                })
+            } else {
+                this.setState({
+                    data: response.data.portfolio_items
+                })
+            }
+        })
           .catch( error => {
             console.log(error);
           })
@@ -65,14 +69,15 @@ export default class PortfolioContainer extends Component {
         };
 
         return (
-            <div className="portfolio-items-wrapper">
-
-                <button className="btn" onClick={() => this.handleFilter("Social Media")}>Social Media</button>
-                <button className="btn" onClick={() => this.handleFilter("School")}>School</button>
-                <button className="btn" onClick={() => this.handleFilter("Storage")}>Storage</button>
-                {this.portfolioItems()}
-
-            </div>            
+            <div className="homepage-wrapper">
+                <div className ="filter-links">                 
+                    <button className="btn" onClick={() => this.handleFilter("Social Media")}>Social Media</button>
+                    <button className="btn" onClick={() => this.handleFilter("School")}>School</button>
+                    <button className="btn" onClick={() => this.handleFilter("Storage")}>Storage</button>            
+                    <button className="btn" onClick={() => this.handleFilter("CLEAR_FILTERS")}>ALL</button>            
+                </div>
+                <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
+            </div>
         );
     }
 }
