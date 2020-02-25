9c5bd699439f48b447d57698fc145047771c95d7
Alexis Flores <alex2chivas@hotmail.com>
2020-02-25 11:32:12 -0800

    [added] information for contact page & new styles for contact

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import contactPageImage from "../../../static/assets/images/auth/login.jpg"
 
 export default function () {
     return (
-        <div>
-            Contact
+        <div className="contact-page-wrapper">
+            <div className="left-column" 
+            style={{
+                backgroundImage: `url(${contactPageImage})`
+            }}
+            />
+
+            <div className="right-column">
+                <div className="contact-bullet-points">
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            <FontAwesomeIcon icon="phone"/>
+                        </div>
+                        <div className="text">
+                            555-555-4545
+                        </div>
+
+                    </div>
+
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            <FontAwesomeIcon icon="envelope"/>
+                        </div>
+                        <div className="text">
+                            alex@icloud.com
+                        </div>
+
+                    </div>
+            
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            <FontAwesomeIcon icon="map-marked-alt"/>
+                        </div>
+                        <div className="text">
+                            Los Angeles, CA
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
