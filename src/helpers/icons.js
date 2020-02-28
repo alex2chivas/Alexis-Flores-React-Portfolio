ff91007a2286a26644a41460a817d4a2a0bb5943
Alexis Flores <alex2chivas@hotmail.com>
2020-02-28 10:55:19 -0800

    [added]: styles for auth page

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -1,8 +1,8 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faTrash, faSignOutAlt, faEdit, faMinusCircle, faSpinner, faPlusSquare, faPhone, faEnvelope, faMapMarkedAlt } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faSignOutAlt, faEdit, faMinusCircle, faSpinner, faPlusSquare, faPhone, faEnvelope, faMapMarkedAlt, faLock } from "@fortawesome/free-solid-svg-icons";
 
 const Icons = () => {
-  return library.add(faTrash, faSignOutAlt, faEdit, faMinusCircle, faSpinner, faPlusSquare, faPhone, faEnvelope, faMapMarkedAlt)
+  return library.add(faTrash, faSignOutAlt, faEdit, faMinusCircle, faSpinner, faPlusSquare, faPhone, faEnvelope, faMapMarkedAlt, faLock)
 }
 
 export default Icons; // NoteTwo // This is a javascript funtion that can be passed anywhere. You can also make a api if you like and pass the 
