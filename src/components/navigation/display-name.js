e709508759bd4bb96da53d2f833e5701f3290e0f
Alexis Flores <alex2chivas@hotmail.com>
2020-04-28 12:22:45 -0700

    added functio nto display my name

--- src/components/navigation/display-name.js
+++ src/components/navigation/display-name.js
@@ -1,23 +1,7 @@
-import React from "react";
+import React from 'react';
 
-onresize = "name()"
-
-function name(){
-  var w = window.outerWidth;
-  var ww = document.documentElement.scrollWidth;
-
-  if (w > 650 && ww > 650) {
-    return (
-      <div className="remove-div">
-        ALEXIS FLORES
-      </div>
-    )
-  } else {
-    return null;
-  }
+function name() {
+	return <div className='remove-div'>ALEXIS FLORES</div>;
 }
 
-
-
 export default name;
-
